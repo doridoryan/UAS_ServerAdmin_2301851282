b0c7be0dbfeda7eee1a688ddd3236d893f8343db
prk <doryankevin@gmail.com>
2022-02-13 03:03:00 -0800

    ini adalah patch untuk konten

--- patching.php
+++ patching.php
@@ -1,17 +1,16 @@
-
 <?php
   
 
     include_once("connect.php"); 
     $query = "SELECT * FROM users";
   
-    if ($result = mysqli_query($con, $query)) {
+    if ($result = mysqli_query($conn, $query)) {
   
     $rowcount = mysqli_num_rows( $result );
       
-    printf("Total rows in this table : %d\n", $rowcount);
+    printf("Total user in this table : %d\n", $rowcount);
 }
   
-mysqli_close($con);
+mysqli_close($conn);
   
 ?>
