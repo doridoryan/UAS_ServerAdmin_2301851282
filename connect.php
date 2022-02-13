2ca5bb0f1de590cb320bdd311e69f71ee379cce8
prk <doryankevin@gmail.com>
2022-02-13 02:50:51 -0800

    ini adalah patch untuk konten

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
  <?php
-	$conn = mysqli_connect("172.23.0.2","root","uas","trucorp-db");
+	$conn = mysqli_connect("172.23.0.3","root","uas","trucorp-db");
     	if($conn -> connect_error){
         	die('Connection to Database Error!'.$conn->error);
     }
---
+++ exe.sh
@@ -0,0 +1,1 @@
+docker cp patching.php trucorp-web-2.0:/var/www/html
